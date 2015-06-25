5dfb71582fc3e5c3c1d21f8bcb23d57fb77f3e02
Pas <pas@gravityrd.com>
2015-06-25 17:28:00 +0200

    no nc in ubuntu:15.04 docker

--- build-in-docker.bash
+++ build-in-docker.bash
@@ -1,9 +1,10 @@
 # for making sure you can bootstrap docker containers fast
 # use apt-cacher-ng (on a host as 172.17.42.1, or on a dedicated IP somewhere in your network)
-# 
+#
 #   sudo docker run --name='apt-cacher-ng' -d -p 3142:3142 -v /opt/apt-cacher-ng:/var/cache/apt-cacher-ng sameersbn/apt-cacher-ng:latest 
 #
-echo 'HEAD / ' | nc 172.17.42.1 3142  && echo 'Acquire::http { Proxy "http://172.17.42.1:3142"; };' > /etc/apt/apt.conf.d/01proxy
+perl -e 'use IO::Socket::INET; $s = new IO::Socket::INET(PeerAddr => "172.17.42.1", PeerPort => "3142", Proto => "tcp", Timeout => 1) || die("no"); $s->send("HEAD /\r\n") || die("nono")' 2>/dev/null \
+    && echo 'Acquire::http { Proxy "http://172.17.42.1:3142"; };' > /etc/apt/apt.conf.d/01proxy
 
 apt-get update
 apt-get install -y --no-install-recommends \
@@ -12,15 +13,20 @@ apt-get install -y --no-install-recommends \
 
 wget --no-check-certificate https://github.com/kevinr/mosh/archive/kevinr-ssh-agent-forwarding.zip && \
 unzip kevinr-ssh-agent-forwarding.zip && \
-cd mosh-kevinr-ssh-agent-forwarding/ 
+cd mosh-kevinr-ssh-agent-forwarding/
 
+dch --nmu "added agent forwarding patch"
 
 if [[ $(g++ --version | grep 4.9 | wc -l ) = 1 ]] ; then
-	apt-get install g++-4.8
-	CXX=g++-4.8 dpkg-buildpackage -uc -us -b
+	apt-get install -y g++-4.8
+	CXX=g++-4.8 dpkg-buildpackage -uc -us -b || echo "Failed to build package :O"
 else
-	dpkg-buildpackage -uc -us -b
+	dpkg-buildpackage -uc -us -b || echo "Failed to build package :O"
 fi
+
 
 mv ../mosh*.deb /opt/
 
+echo "Finished"
+
+ls -alh /opt/
